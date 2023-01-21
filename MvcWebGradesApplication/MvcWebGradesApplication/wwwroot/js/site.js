55fc974b2184d2685b9e70c808cd5b6cf2c4333f
Marcelo Amorim <marceloamorim.fa@gmail.com>
2023-01-21 23:38:20 +0000

    Ordenar e paginar a lista de formandos

--- MvcWebGradesApplication/MvcWebGradesApplication/wwwroot/js/site.js
+++ MvcWebGradesApplication/MvcWebGradesApplication/wwwroot/js/site.js
@@ -6,3 +6,34 @@
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
+
+$(document).ready(function () {
+    $('#tabela-formandos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registo encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registos",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registos",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registos)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registos por página",
+            "sLoadingRecords": "Carregando os dados ...",
+            "sProcessing": "Processando os dados ...",
+            "sZeroRecords": "Nenhum registo encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Seguinte",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
